1278568bf53e7da7c1e9a327448587ea38ec4890
George <brayanher2000@hotmail.com>
2025-05-07 18:08:09 -0400

    Added new Icons and colors to it

--- src/views/components/load_quicklinks.php
+++ src/views/components/load_quicklinks.php
@@ -8,3 +8,29 @@ $links = [];
 if ($user_id) {
     $links = getUserQuickLinks($user_id);
 }
+
+function getIconStyles($iconClass) {
+    $styles = [
+        'fa-youtube' => 'text-red-600 text-2xl',
+        'fa-spotify' => 'text-green-500 text-2xl',
+        'fa-envelope' => 'text-gray-600 text-2xl',
+        'fa-google' => 'text-blue-500 text-2xl',
+        'fa-book' => 'text-yellow-700 text-2xl',
+        'fa-github' => 'text-black text-2xl',
+        'fa-calendar-days' => 'text-indigo-500 text-2xl',
+        'fa-file-word' => 'text-blue-700 text-2xl',
+        'fa-linkedin' => 'text-blue-800 text-2xl',
+        'fa-whatsapp' => 'text-green-600 text-2xl',
+        'fa-discord' => 'text-indigo-400 text-2xl',
+        'fa-x-twitter' => 'text-black text-2xl',
+        'fa-reddit' => 'text-orange-500 text-2xl'
+    ];
+
+    foreach ($styles as $key => $value) {
+        if (str_contains($iconClass, $key)) {
+            return $value;
+        }
+    }
+
+    return 'text-gray-700 text-2xl'; // Default styling
+}
