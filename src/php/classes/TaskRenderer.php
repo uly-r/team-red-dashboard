e8c56ed801bbc11e2b027459d497f0b0009220b7
uly-r <ulyredr@gmail.com>
2025-05-16 21:32:16 -0400

    added escaping to prevent the edit button from breaking

--- src/php/classes/TaskRenderer.php
+++ src/php/classes/TaskRenderer.php
@@ -17,18 +17,20 @@ class TaskRenderer {
 
     public static function renderEditButton($task) {
         //this loads the current task information into the form
+        /* JSON_HEX_TAG escapes <> and JSON_HEX_QUOT escapes double quotes
+        used here to prevent the program breaking */
         $id = (int)$task['id'];
-        $title = json_encode($task['title']);
-        $desc = json_encode($task['description']);
-        $status = json_encode((string)$task['is_completed']);// Cast to string for form
-        $due = json_encode($task['due_date']);
-        $priority = json_encode($task['task_priority']);
+        $title = json_encode($task['title'],JSON_HEX_TAG | JSON_HEX_QUOT);
+        $desc = json_encode($task['description'],JSON_HEX_TAG | JSON_HEX_QUOT);
+        $status = json_encode((string)$task['is_completed'],JSON_HEX_TAG | JSON_HEX_QUOT);// Cast to string for form
+        $due = json_encode($task['due_date'],JSON_HEX_TAG | JSON_HEX_QUOT);
+        $priority = json_encode($task['task_priority'],JSON_HEX_TAG | JSON_HEX_QUOT);
 
-        return "
-            <button onclick='openEditForm($id, $title, $desc, $status, $due, $priority)'
-                class='text-blue-500 hover:text-blue-700'>
-                <i class='fa-solid fa-pen'></i>
-            </button>";
+        $onclick= "openEditForm($id, $title, $desc, $status, $due, $priority)";
+        $safeOnclick = htmlspecialchars($onclick, ENT_QUOTES, 'UTF-8');
+
+        // the "\" in "<button onclick=\"$safeOnclick\" treats safeOnClick as part of the string
+        return "<button onclick=\"$safeOnclick\" class='text-blue-500 hover:text-blue-700'><i class='fa-solid fa-pen'></i></button>";
     }
 
     // delete button, pops a confirmation for the user before deletion
