bb48ea979fc19641b7258e21ccad06658f728232
George <brayanher2000@hotmail.com>
2025-05-16 20:16:10 -0400

    fixed style in calender

--- src/views/components/calender.php
+++ src/views/components/calender.php
@@ -67,7 +67,7 @@ while ($row = $result->fetch_assoc()) {
                         onclick="selectDate(this)" 
                         class="h-8 flex items-center justify-center rounded-full cursor-pointer ' .
                         ($isToday ? 'bg-blue-500 text-white ' : '') .
-                        ($hasTask ? 'bg-yellow-300 hover:bg-yellow-400 ' : 'hover:bg-yellow-100') .
+                        ($hasTask ? 'bg-yellow-300 hover:bg-yellow-400 ' : 'hover:bg-blue-700') .
                         '" title="' . htmlspecialchars($taskTitles) . '">' . $i . 
                         '</div>';
                 }               
